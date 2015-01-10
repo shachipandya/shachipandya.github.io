2acf8f8250c60adcbf2e60aabda8235d074a999b
Shachi Pandya <hardik@hardik.org>
2015-01-10 11:38:26 +0530

    Removed designer blouses.

--- js/script.js
+++ js/script.js
@@ -1,19 +1,13 @@
 
 setTimeout(function(){
   $("#rotated-roles p").fadeOut(function(){
-    $(this).text("Designer Kurtis / Chaniya Choli / Sarees / Designer Gowns / Designer Blouses").fadeIn();
-
-  });}, 7500);
-
-setTimeout(function(){
-  $("#rotated-roles p").fadeOut(function(){
-    $(this).text("Designer Blouses").fadeIn();
+    $(this).text("Designer Kurtis / Chaniya Choli / Sarees / Designer Blouses").fadeIn();
 
   });}, 6000);
 
   setTimeout(function(){
   $("#rotated-roles p").fadeOut(function(){
-    $(this).text("Designer Gowns").fadeIn();
+    $(this).text("Designer Blouses").fadeIn();
 
   });}, 4500);
 
